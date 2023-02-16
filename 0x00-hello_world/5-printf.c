f3eac54af0f569d125878238ee0f5ca3c8c7535e
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-16 13:49:16 -0800

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Author: Raphael Miyoyo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - outputs "with proper grammar, but the outcome 
+ *          is a piece of art,\n" and a newline.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
