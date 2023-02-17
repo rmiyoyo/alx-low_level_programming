5ee7695fdccce4a96bfcb517d5cdd54f044dc952
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-17 15:04:52 -0800

    comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -13,24 +13,24 @@
  */
 int main(void)
 {
-	int number1, number2;
+	int a, b;
 
-	for (number1 = 0; number1 < 9; number1++)
+	for (a= 48; a<= 56; a++)
 	{
-		for (number2 = number1 + 1; number2 < 10; number2++)
+		for (b = 49; b <= 57; b++)
 		{
-			putchar((number1 % 10) + '0');
-			putchar((number2 % 10) + '0');
-
-			if (number1 == 8 && number2 == 9)
-				continue;
-			
-			putchar(',');
-			putchar(' ');
+			if (b > a)
+			{
+				putchar(a);
+				putchar(b);
+				if (a!= 56 || b != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
