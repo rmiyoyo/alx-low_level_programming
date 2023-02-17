f2198adf9c03bcd263d2b94645f85c32254cdb76
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-17 15:09:10 -0800

    comb 3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,30 +7,31 @@
 
 /**
  * main - Outputs all possible combinations of two different digits,
- *        in ascending order, separated by a , followed by a space.
+ *        in ascending order, separated by a comma followed by a space
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	int a, b;
+    int a, b;
 
-	for (a= 48; a<= 56; a++)
-	{
-		for (b = 49; b <= 57; b++)
-		{
-			if (b > a)
-			{
-				putchar(a);
-				putchar(b);
-				if (a!= 56 || b != 57)
-				{
-					putchar(',');
-					putchar(' ');
-				}
-			}
-		}
-	}
-	putchar('\n');
-	return (0);
+    for (a = 48; a <= 56; a++)
+    {
+        for (b = 49; b <= 57; b++)
+        {
+            if (b > a)
+            {
+                putchar(a);
+                putchar(b);
+                if (a != 56 || b != 57)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    putchar('\n');
+
+    return (0);
 }
