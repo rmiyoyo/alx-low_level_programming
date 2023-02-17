f41653c08ec50cec01284ba3a729aadcdd281abf
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-17 15:32:03 -0800

    comb 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+/*
+ * C File: 102-print_comb5.c
+ * Author: Raphael Miyoyo
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all possible combinations of two two-digit numbers
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a < 100; a++)
+	{
+		for (b = 0; b < 100; b++)
+		{
+			if (a < b)
+			{
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
+				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
