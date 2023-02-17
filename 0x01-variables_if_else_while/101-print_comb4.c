0f488e1ce4e3cd6361f4b2427b76c5f2905d9824
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-17 15:25:12 -0800

    comb 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,40 @@
+/*
+ * C File: 101-print_comb4.c
+ * Author: Raphael Miyoyo
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Outputs all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int number1, number2, number3;
+
+	for (number1 = 0; number1 < 8; number1++)
+	{
+		for (number2 = number1 + 1; number2 < 9; number2++)
+		{
+			for (number3 = number2 + 1; number3 < 10; number3++)
+			{
+				putchar((number1 % 10) + '0');
+				putchar((number2 % 10) + '0');
+				putchar((number3 % 10) + '0');
+
+				if (number1 == 7 && number2 == 8 && number3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
