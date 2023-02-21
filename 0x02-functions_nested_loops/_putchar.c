b665b882c8a1bd7cbcf90f2a1daa6a5b3c5a6251
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-21 15:23:47 -0800

    final

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - Outputs the character c to stdout
+ * '@c': The character to print
+ *
+ * Return: On success 0.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
