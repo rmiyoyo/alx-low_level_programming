f78d6c7dbb5033a9b8e993cb1ed436caafc535a6
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-22 01:44:27 -0800

    two fibonacci implementation

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,44 +2,28 @@
 #include "main.h"
 
 /**
- * main - fibonacci <3
- * Purpose - no hardcode
- * Return: (Success)
+ * main - a function that print the first 50 fibonacci numbers.
+ * Return: Nothing.
  */
-
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
+	unsigned long count, i, j, k;
 
-	printf("%lu", bef);
+	i = 0;
+	j = 1;
 
-	for (i = 1; i < 91; i++)
+	for (count = 0; count < 50; count++)
 	{
-		printf(", %lu", aft);
-		aft += bef;
-		bef = aft - bef;
+		k = i + j;
+		i = j;
+		j = k;
+		printf("%lu", k);
+		if (count == 49)
+			putchar('\n');
+		else
+		{
+			printf(", ");
+		}
 	}
-	bef1 = (bef / l);
-	bef2 = (bef % l);
-	aft1 = (aft / l);
-	aft2 = (aft % l);
-
-	for (i = 92; i < 99; ++i)
-	{
-		printf(", %lu", aft1 + (aft2 / l));
-		printf("%lu", aft2 % l);
-		aft1 = aft1 + bef1;
-		bef1 = aft1 - bef1;
-		aft2 = aft2 + bef2;
-		bef2 = aft2 - bef2;
-	}
-	printf("\n");
 	return (0);
 }
