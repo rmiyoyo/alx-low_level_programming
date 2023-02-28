79467176983d309d594a23d56de7706293048a82
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-02-27 16:22:08 -0800

    puts half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * puts_half - print half of a string.
+ * @str: string to process.
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+
+	while (*(str + len) != '\0')
+	{
+		len++;
+	}
+	if (!(len % 2))
+		len /= 2;
+	else
+		len = (len + 1) / 2;
+
+	while (*(str + len) != '\0')
+	{
+		_putchar(*(str + len));
+		len++;
+	}
+	_putchar('\n');
+}
+
