cbc8685b802411aa868db40699feef293a316d46
Raphael Miyoyo <miyoyoraphael@gmail.com>
2023-03-03 08:26:27 -0800

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,49 +1,29 @@
 #include "main.h"
 /**
- * print_number - print integer
- * Return: void
- * @n: number
+ * rot13 - rot13 encoding
+ * Return: pointer to arr
+ * @s: string
  */
-void print_number(int n)
+char *rot13(char *s)
 {
-	unsigned int size = n;
+	char half1[] = "aAbBcCdDeEfFgGhHiIjJkKlLmMnNoOpPqQrRsStTuUvVwWxXyYzZ";
 
-	int digits = 0;
+	char half2[] = "nNoOpPqQrRsStTuUvVwWxXyYzZaAbBcCdDeEfFgGhHiIjJkKlLmM";
 
-	int i;
+	int i = 0, j;
 
-	if (n < 0)
-		size = -n;
-	if (n == 0)
-		digits = 1;
-
-	while (size >= 1)
+	while (s[i] != 0)
 	{
-		size = size / 10;
-		digits++;
-	}
+		char c = s[i];
 
-	for (i = 0; i < digits; i++)
-	{
-
-		int pow = 1;
-
-		int j;
-
-		int d;
-
-		for (j = 0; j < digits - i - 1; j++)
+		for (j = 0; j < 52; j++)
 		{
-			pow = pow * 10;
+			if (c == half1[j])
+			{
+				s[i] = half2[j];
+			}
 		}
-
-		d = ((n / pow) % 10);
-		if (n < 0)
-		{
-			d = -d;
-			if (i == 0)
-				_putchar(45);
-		}
-		_putchar(48 + d);
+		i++;
 	}
+	return (s);
 }
